2e68c21fb778101c17856b6e31ce46ef65a9954b
Leonard A. Harris <lh64@cornell.edu>
2020-06-03 05:46:24 -0500

    Added code for looping over percentages of variable sites. Also made file path OS independent in 'multihit_sim.py'.

--- make_plots.py
+++ make_plots.py
@@ -3,168 +3,197 @@ import matplotlib.pyplot as plt
 import os
 
 SUFFIX = ['_5strains', '_50strains']
-EC = ['black', 'blue']
+EC = ['black', 'black']
 FC = ['0.75','None']
 LW = [1,2]
 LABEL = ['5 strains', '50 strains']
+PCT_VAR = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 def make_plots(SUFFIX,EC,FC,LW,LABEL):
     
-    for suffix,ec,fc,lw,label in zip(SUFFIX,EC,FC,LW,LABEL):
-        
-#         data_exp = np.genfromtxt('INPUT/gene_length_mut%s.csv' % suffix, 
-#                              dtype=None, delimiter=',', names=True, encoding='UTF-8')
-        filename = os.path.join(os.path.curdir, 'INPUT', 'gene_length_mut%s.csv' % suffix)
-        data_exp = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
-                                 encoding='UTF-8')
-        
-        
-        HO_mut_exp = [(d['HSmut'], d['Convergent'], d['Parallel'], d['Coincidental']) 
-                  for d in data_exp if d['Orientation'] == 'HO' and d['Length'] > 200]
-        CD_mut_exp = [(d['HSmut'], d['Convergent'], d['Parallel'], d['Coincidental']) 
-                  for d in data_exp if d['Orientation'] == 'CD' and d['Length'] > 200]
-        
-        n_ho_genes = len(HO_mut_exp)
-        n_cd_genes = len(CD_mut_exp)
-                
-        ho_hsmut_exp = [x[0] for x in HO_mut_exp if x[0]>0]
-        cd_hsmut_exp = [x[0] for x in CD_mut_exp if x[0]>0]
-        
-        print(suffix)
-        print(sum(ho_hsmut_exp), n_ho_genes, '%.2f' % (sum(ho_hsmut_exp)/n_ho_genes))
-        print(sum(cd_hsmut_exp), n_cd_genes, '%.2f' % (sum(cd_hsmut_exp)/n_cd_genes))
-        
-        ratio_sites_exp = sum(ho_hsmut_exp)/sum(cd_hsmut_exp)
-        ratio_genes_exp = len(ho_hsmut_exp)/len(cd_hsmut_exp)
-        
-        filename = os.path.join(os.path.curdir, 'OUTPUT', 'hsmut_sites%s.csv' % suffix)
-        data_sim_sites = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
-                                       encoding='UTF-8')
-        filename = os.path.join(os.path.curdir, 'OUTPUT', 'hsmut_genes%s.csv' % suffix)
-        data_sim_genes = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
-                                       encoding='UTF-8')
-        
-#         data_sim_sites = np.genfromtxt('OUTPUT/hsmut_sites%s.csv' % suffix, 
-#                             dtype=None, delimiter=',', names=True, encoding='UTF-8')
-#         data_sim_genes = np.genfromtxt('OUTPUT/hsmut_genes%s.csv' % suffix, 
-#                             dtype=None, delimiter=',', names=True, encoding='UTF-8')
-        
-        ho_hsmut_sites_sim = [d[0] for d in data_sim_sites]
-        cd_hsmut_sites_sim = [d[1] for d in data_sim_sites]
-        ratio_sites_sim = [d[0]/d[1] for d in data_sim_sites]
-        
-        ho_hsmut_genes_sim = [d[0] for d in data_sim_genes]
-        cd_hsmut_genes_sim = [d[1] for d in data_sim_genes]
-        ratio_genes_sim = [d[0]/d[1] for d in data_sim_genes]
-
-        plt.figure('R_N')
-        dx = 0.025 #0.1
-#         bins = np.arange(0, 2.5+dx, dx)
-        bins = np.arange(0, 0.8+dx, dx)
-        plt.hist(ratio_sites_sim, bins=bins, density=False, 
-                 weights=[1/len(ratio_sites_sim)]*len(ratio_sites_sim), 
-                 color='white', ec=ec, fc=fc, lw=lw, label=label)
-        plt.plot([ratio_sites_exp], [0.2], '*', ms=12, mec=fc, mfc=ec, label='observed')
-        plt.xlabel(r'$R_N$')
-        plt.ylabel('frequency')
-#         plt.xticks([bins[i] for i in range(len(bins)) if np.mod(i,4)==0])
-        plt.ylim(ymax=0.5) #0.5
-        plt.legend(loc=0)
-
-        plt.figure('R_G')
-        dx = 0.025 #0.1
-#         bins = np.arange(0, 2+dx, dx)
-        bins = np.arange(0, 0.8+dx, dx)
-        plt.hist(ratio_genes_sim, bins=bins, density=False, 
-                 weights=[1/len(ratio_genes_sim)]*len(ratio_genes_sim), 
-                 color='white', ec=ec, fc=fc, lw=lw, label=label)
-        plt.plot([ratio_genes_exp], [0.2], '*', ms=12, mec=fc, mfc=ec, label='observed')
-        plt.xlabel(r'$R_G$')
-        plt.ylabel('frequency')
-#         plt.xticks([bins[i] for i in range(len(bins)) if np.mod(i,4)==0])
-        plt.ylim(ymax=0.5) #0.5
-        plt.legend(loc=0)
-        
-        def get_bins(x, y):
-            dmin = min(min(x),min(y))
-            dmax = max(max(x),max(y))
-            order = 0
-            n = 2
-            while dmax > 2*10**(order+(order+1)*n):
-                order += 1
-            step = 10**order
-            if dmin % 10*step != 0:
-                dmin = int(round((dmin-5*step)/(10*step))*(10*step))
-            if dmax % 10*step != 0:
-                dmax = int(round((dmax+5*step)/(10*step))*(10*step))
-            bins = range(dmin,dmax,step)
-            return bins
-        
-        bins = get_bins(ho_hsmut_sites_sim, cd_hsmut_sites_sim)
-        
-        plt.figure('hsmut_sites%s' % suffix)
-        plt.hist(ho_hsmut_sites_sim, bins=bins, color='white', fc='0.75', 
-                 ec='red', lw=1, label='simulated HO')
-        plt.hist(cd_hsmut_sites_sim, bins=bins, color='white', fc='None', 
-                 ec='blue', lw=2, label='simulated CD')
-        plt.annotate('Observed HO = {:,}'.format(sum(ho_hsmut_exp)) , xy=(0.6,0.75), 
-                     xycoords='axes fraction', weight='bold', color='red')
-        plt.annotate('Observed CD = {:,}'.format(sum(cd_hsmut_exp)), xy=(0.6,0.69), 
-                     xycoords='axes fraction', weight='bold', color='blue')
-        plt.xlabel('# multi-hit sites') 
-        plt.ylabel('number')
-        # plt.xticks(range(0,11))
-        plt.legend(loc=0)        
-        
-        bins = get_bins(ho_hsmut_genes_sim, cd_hsmut_genes_sim)
-        
-        plt.figure('hsmut_genes%s' % suffix)
-        plt.hist(ho_hsmut_genes_sim, bins=bins, color='white', fc='0.75', 
-                 ec='red', lw=1, label='simulated HO')
-        plt.hist(cd_hsmut_genes_sim, bins=bins, color='white', fc='None', 
-                 ec='blue', lw=2, label='simulated CD')
-        plt.annotate('Observed HO = {:,}'.format(len(ho_hsmut_exp)) , xy=(0.6,0.75), 
-                     xycoords='axes fraction', weight='bold', color='red')
-        plt.annotate('Observed CD = {:,}'.format(len(cd_hsmut_exp)), xy=(0.6,0.69), 
-                     xycoords='axes fraction', weight='bold', color='blue')
-        plt.xlabel('# multi-hit genes') 
-        plt.ylabel('number')
-        # plt.xticks(range(0,11))
-        plt.legend(loc=0)    
-        
-        ##### PARALLEL MUTATIONS #####
-        
-        ho_par_exp = [x[2] for x in HO_mut_exp if x[2]>0]
-        cd_par_exp = [x[2] for x in CD_mut_exp if x[2]>0]
-        
-        ratio_sites_par_exp = sum(ho_par_exp)/sum(cd_par_exp)
-        ratio_genes_par_exp = len(ho_par_exp)/len(cd_par_exp)
-        
-        filename = os.path.join(os.path.curdir, 'OUTPUT', 'parmut%s.csv' % suffix)
-        data_sim_par = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
-                                     encoding='UTF-8')
-        
-#         data_sim_par = np.genfromtxt('OUTPUT/sum_parmut%s.csv' % suffix, dtype=None, 
-#                                      delimiter=',', names=True, encoding='UTF-8')
-        
-        sum_par_HO = [d[0] for d in data_sim_par]
-        sum_par_CD = [d[1] for d in data_sim_par]
-        
-        plt.figure('parmut%s' % suffix)
-        bins = get_bins(sum_par_HO, sum_par_CD)
-        plt.hist(sum_par_HO, bins=bins, color='white', fc='0.75', 
-                 ec='red', lw=1, label='simulated HO')
-        plt.hist(sum_par_CD, bins=bins, color='white', fc='None', 
-                 ec='blue', lw=2, label='simulated CD')
-        plt.annotate('Observed HO = {:,}'.format(sum(ho_par_exp)) , xy=(0.6,0.75), 
-                     xycoords='axes fraction', weight='bold', color='red')
-        plt.annotate('Observed CD = {:,}'.format(sum(cd_par_exp)), xy=(0.6,0.69), 
-                     xycoords='axes fraction', weight='bold', color='blue')
-        plt.xlabel('# parallel mutations') 
-        plt.ylabel('number')
-        # plt.xticks(range(0,11))
-        plt.legend(loc=0)
+    for pct in PCT_VAR:
+            
+        print(pct)
     
-    plt.show()
+        for suffix,ec,fc,lw,label in zip(SUFFIX,EC,FC,LW,LABEL):
+            
+            print(suffix)
+ 
+            ##### MULTI-HIT MUTATIONS #####
+    
+            filename = os.path.join(os.path.curdir, 'SIM_INPUT', 
+                                    'gene_length_mut%s.csv' % suffix)
+            data_exp = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
+                                     encoding='UTF-8')
+            
+            HO_mut_exp = [(d['HSmut'], d['Convergent'], d['Parallel'], d['Coincidental']) 
+                      for d in data_exp if d['Orientation'] == 'HO' and d['Length'] > 200]
+            CD_mut_exp = [(d['HSmut'], d['Convergent'], d['Parallel'], d['Coincidental']) 
+                      for d in data_exp if d['Orientation'] == 'CD' and d['Length'] > 200]
+            
+            n_ho_genes = len(HO_mut_exp)
+            n_cd_genes = len(CD_mut_exp)
+                    
+            ho_hsmut_exp = [x[0] for x in HO_mut_exp if x[0]>0]
+            cd_hsmut_exp = [x[0] for x in CD_mut_exp if x[0]>0]
+            
+            ratio_sites_exp = sum(ho_hsmut_exp)/sum(cd_hsmut_exp)
+            ratio_genes_exp = len(ho_hsmut_exp)/len(cd_hsmut_exp)
+            
+            filename = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                                    'hsmut_sites%s_%d.csv' % (suffix,pct))
+            data_sim_sites = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
+                                           encoding='UTF-8')
+            filename = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                                    'hsmut_genes%s_%d.csv' % (suffix,pct))
+            data_sim_genes = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
+                                           encoding='UTF-8')
+            
+            ho_hsmut_sites_sim = [d[0] for d in data_sim_sites]
+            cd_hsmut_sites_sim = [d[1] for d in data_sim_sites]
+            ratio_sites_sim = [d[0]/d[1] for d in data_sim_sites]
+            
+            ho_hsmut_genes_sim = [d[0] for d in data_sim_genes]
+            cd_hsmut_genes_sim = [d[1] for d in data_sim_genes]
+            ratio_genes_sim = [d[0]/d[1] for d in data_sim_genes]
+            
+            def get_pval(sim_data, exp_val):
+                p_geq = len([r for r in sim_data if r >= exp_val]) / \
+                    len(sim_data)
+                p_leq = len([r for r in sim_data if r <= exp_val]) / \
+                    len(sim_data)
+                return 2*min(p_geq, p_leq) #two-tailed p-value
+            
+            plt.figure('R_N_%d' % pct)
+            dx = 0.025 #0.1
+            bins = np.arange(0, 0.8+dx, dx)
+            plt.hist(ratio_sites_sim, bins=bins, density=False, 
+                     weights=[1/len(ratio_sites_sim)]*len(ratio_sites_sim), 
+                     color='white', ec=ec, fc=fc, lw=lw, label=label)
+            p_value = get_pval(ratio_sites_sim, ratio_sites_exp)
+            plt.plot([ratio_sites_exp], [0.3], '*', ms=12, mec=ec, mfc=fc, 
+                     label='Observed (p = %.2f)' % p_value)
+            plt.xlabel(r'$R_N$')
+            plt.ylabel('frequency')
+            plt.legend(loc=0)
+    
+            plt.figure('R_G_%d' % pct)
+            dx = 0.025 #0.1
+            bins = np.arange(0, 0.8+dx, dx)
+            plt.hist(ratio_genes_sim, bins=bins, density=False, 
+                     weights=[1/len(ratio_genes_sim)]*len(ratio_genes_sim), 
+                     color='white', ec=ec, fc=fc, lw=lw, label=label)
+            p_value = get_pval(ratio_genes_sim, ratio_genes_exp)
+            plt.plot([ratio_genes_exp], [0.3], '*', ms=12, mec=ec, mfc=fc, 
+                     label='Observed (p = %.2f)' % p_value)
+            plt.xlabel(r'$R_G$')
+            plt.ylabel('frequency')
+            plt.legend(loc=0)
+            
+            def get_bins(x, y):
+                dmin = min(min(x),min(y))
+                dmax = max(max(x),max(y))
+                order = 0
+                n = 2
+                while dmax > 2*10**(order+(order+1)*n):
+                    order += 1
+                step = 10**order
+                if dmin % 10*step != 0:
+                    dmin = int(round((dmin-5*step)/(10*step))*(10*step))
+                if dmax % 10*step != 0:
+                    dmax = int(round((dmax+5*step)/(10*step))*(10*step))
+                bins = range(dmin,dmax,step)
+                return bins
+            
+            plt.figure('hsmut_sites%s_%d' % (suffix,pct))
+            bins = get_bins(ho_hsmut_sites_sim, cd_hsmut_sites_sim)
+            plt.hist(ho_hsmut_sites_sim, bins=bins, color='white', fc='0.75', 
+                     ec='red', lw=1, label='simulated HO')
+            plt.hist(cd_hsmut_sites_sim, bins=bins, color='white', fc='None', 
+                     ec='blue', lw=2, label='simulated CD')
+            p_value_ho = get_pval(ho_hsmut_sites_sim, sum(ho_hsmut_exp))
+            p_value_cd = get_pval(cd_hsmut_sites_sim, sum(cd_hsmut_exp))
+            plt.annotate('Observed HO = {:,} (p = {:.4g})'.format(sum(ho_hsmut_exp), p_value_ho), 
+                         xy=(0.4,0.75), xycoords='axes fraction', weight='bold', color='red')
+            plt.annotate('Observed CD = {:,} (p = {:.4g})'.format(sum(cd_hsmut_exp), p_value_cd), 
+                         xy=(0.4,0.69), xycoords='axes fraction', weight='bold', color='blue')
+            plt.xlabel('# multi-hit sites') 
+            plt.ylabel('number')
+            plt.legend(loc=0)      
+            
+            outfile = os.path.join(os.path.curdir, 'PLOTS', 
+                                   'hsmut_sites%s_%d.pdf' % (suffix,pct))
+            plt.savefig(outfile, format='pdf') 
+    
+            plt.figure('hsmut_genes%s_%d' % (suffix,pct))
+            bins = get_bins(ho_hsmut_genes_sim, cd_hsmut_genes_sim)
+            plt.hist(ho_hsmut_genes_sim, bins=bins, color='white', fc='0.75', 
+                     ec='red', lw=1, label='simulated HO')
+            plt.hist(cd_hsmut_genes_sim, bins=bins, color='white', fc='None', 
+                     ec='blue', lw=2, label='simulated CD')
+            p_value_ho = get_pval(ho_hsmut_genes_sim, len(ho_hsmut_exp))
+            p_value_cd = get_pval(cd_hsmut_genes_sim, len(cd_hsmut_exp))
+            plt.annotate('Observed HO = {:,} (p = {:.4g})'.format(len(ho_hsmut_exp), p_value_ho), 
+                         xy=(0.4,0.75), xycoords='axes fraction', weight='bold', color='red')
+            plt.annotate('Observed CD = {:,} (p = {:.4g})'.format(len(cd_hsmut_exp), p_value_cd), 
+                         xy=(0.4,0.69), 
+                         xycoords='axes fraction', weight='bold', color='blue')
+            plt.xlabel('# multi-hit genes') 
+            plt.ylabel('number')
+            plt.legend(loc=0)
+            
+            outfile = os.path.join(os.path.curdir, 'PLOTS', 
+                                   'hsmut_genes%s_%d.pdf' % (suffix,pct))
+            plt.savefig(outfile, format='pdf')   
+            
+            ##### PARALLEL MUTATIONS #####
+            
+            ho_par_exp = [x[2] for x in HO_mut_exp if x[2]>0]
+            cd_par_exp = [x[2] for x in CD_mut_exp if x[2]>0]
+            
+            ratio_sites_par_exp = sum(ho_par_exp)/sum(cd_par_exp)
+            ratio_genes_par_exp = len(ho_par_exp)/len(cd_par_exp)
+            
+            filename = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                                    'parmut%s_%d.csv' % (suffix,pct))
+            data_sim_par = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, 
+                                         encoding='UTF-8')
+            
+            sum_par_HO = [d[0] for d in data_sim_par]
+            sum_par_CD = [d[1] for d in data_sim_par]
+            
+            plt.figure('parmut%s_%d' % (suffix,pct))
+            bins = get_bins(sum_par_HO, sum_par_CD)
+            plt.hist(sum_par_HO, bins=bins, color='white', fc='0.75', 
+                     ec='red', lw=1, label='simulated HO')
+            plt.hist(sum_par_CD, bins=bins, color='white', fc='None', 
+                     ec='blue', lw=2, label='simulated CD')
+            p_value_ho = get_pval(sum_par_HO, sum(ho_par_exp))
+            p_value_cd = get_pval(sum_par_CD, sum(cd_par_exp))
+            plt.annotate('Observed HO = {:,} (p = {:.4g})'.format(sum(ho_par_exp), p_value_ho), 
+                         xy=(0.4,0.75), xycoords='axes fraction', weight='bold', color='red')
+            plt.annotate('Observed CD = {:,} (p = {:.4g})'.format(sum(cd_par_exp), p_value_cd), 
+                         xy=(0.4,0.69), xycoords='axes fraction', weight='bold', color='blue')
+            plt.xlabel('# parallel mutations') 
+            plt.ylabel('number')
+            plt.legend(loc=0)
+            
+            outfile = os.path.join(os.path.curdir, 'PLOTS', 
+                                   'parmut%s_%d.pdf' % (suffix,pct))
+            plt.savefig(outfile, format='pdf')
+        
+        plt.figure('R_N_%d' % pct)
+        ymax = round(plt.ylim()[1]*10+0.5)/10
+        plt.ylim(ymax=ymax) #0.5
+        outfile = os.path.join(os.path.curdir, 'PLOTS', 'multihit_RN_%d.pdf' % pct)
+        plt.savefig(outfile, format='pdf')
+        
+        plt.figure('R_G_%d' % pct)
+        ymax = round(plt.ylim()[1]*10+0.5)/10
+        plt.ylim(ymax=ymax) #0.5
+        outfile = os.path.join(os.path.curdir, 'PLOTS', 'multihit_RG_%d.pdf' % pct)
+        plt.savefig(outfile, format='pdf')
+        
+#         plt.show()
     
 make_plots(SUFFIX, EC, FC, LW, LABEL)
--- multihit_sim.py
+++ multihit_sim.py
@@ -1,78 +1,95 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import os
 
-filename = 'INPUT/gene_length_mut_5strains.csv'
+filename = os.path.join(os.path.curdir, 'SIM_INPUT', 'gene_length_mut_50strains.csv')
 suffix = re.match(".+mut(.*)\.csv",filename).group(1)
 data = np.genfromtxt(filename, dtype=None, delimiter=',', names=True, encoding='UTF-8')
 
-print(data)
 print(data.dtype.names)
 
-HO_nonsyn = [(d['Length'], 2*d['Nonsyn']) for d in data 
+HO_nonsyn = [(d['Length'], d['Nonsyn']) for d in data 
              if d['Orientation'] == 'HO' and d['Length'] > 200]
 CD_nonsyn = [(d['Length'], d['Nonsyn']) for d in data 
              if d['Orientation'] == 'CD' and d['Length'] > 200]
 
+## HO and CD genes have same length distributions 
+'''
+bins = np.arange(0,2001,100)
+plt.hist([ho[0] for ho in HO_nonsyn], bins=bins, label='HO', 
+         weights=[1/len(HO_nonsyn)]*len(HO_nonsyn))
+plt.hist([cd[0] for cd in CD_nonsyn], bins=bins, label='CD', fc='None', ec='k', lw=2,
+         weights=[1/len(CD_nonsyn)]*len(CD_nonsyn))
+plt.show()
+'''
+
 np.random.seed(0)
 
-niter = 10000
+# percentage of variable sites
+pct_var = np.arange(0.1, 1.1, 0.1)
 
-sum_hsmut_sites_HO = [0]*niter
-sum_hsmut_sites_CD = [0]*niter
-sum_hsmut_genes_HO = [0]*niter
-sum_hsmut_genes_CD = [0]*niter
-sum_parmut_HO  = [0]*niter
-sum_parmut_CD  = [0]*niter
-
-for n in range(niter):
-    print(n)
-    sum_hsmut_sites = [0,0] # [HO,CD]
-    sum_hsmut_genes = [0,0]
-    sum_parmut = [0,0]
-    for i,type in enumerate([HO_nonsyn, CD_nonsyn]):
-        for gene in type:
-            x = np.random.randint(gene[0], size=gene[1])
-            pos, hits = np.unique(x, return_counts=True)
-            '''
-            while (len(pos) < gene[1]):
-                x = np.append(x, np.random.randint(gene[0], size=gene[1]-len(pos)))
+for pct in pct_var:
+    
+    niter = 10000
+    
+    sum_hsmut_sites_HO = [0]*niter
+    sum_hsmut_sites_CD = [0]*niter
+    sum_hsmut_genes_HO = [0]*niter
+    sum_hsmut_genes_CD = [0]*niter
+    sum_parmut_HO  = [0]*niter
+    sum_parmut_CD  = [0]*niter
+    
+    for n in range(niter):
+        print(n)
+        sum_hsmut_sites = [0,0] # [HO,CD]
+        sum_hsmut_genes = [0,0]
+        sum_parmut = [0,0]
+        for i,type in enumerate([HO_nonsyn, CD_nonsyn]):
+            for gene in type:
+                nsites = int(round(gene[0]*pct))
+                x = np.random.randint(nsites, size=gene[1])
                 pos, hits = np.unique(x, return_counts=True)
-            '''
-            multihits = [h for h in hits if h > 1]
-            n_multihits = len(multihits)
-
-            if n_multihits > 0:
-                sum_hsmut_sites[i] += n_multihits
-                sum_hsmut_genes[i] += 1
-                # Check for parallel mutations
-                for mh in multihits:
-                    # 20 amino acids to choose from (there are 21 total)
-                    xx = np.random.randint(20, size=mh) 
-                    aa, nmut = np.unique(xx, return_counts=True)
-                    parallel = [nm for nm in nmut if nm > 1]
-                    n_parallel = len(parallel)
-                    if n_parallel > 0:
-                        sum_parmut[i] += n_parallel
-            
-    sum_hsmut_sites_HO[n] = sum_hsmut_sites[0]
-    sum_hsmut_sites_CD[n] = sum_hsmut_sites[1]
-    sum_hsmut_genes_HO[n] = sum_hsmut_genes[0]
-    sum_hsmut_genes_CD[n] = sum_hsmut_genes[1]
-    sum_parmut_HO[n]  = sum_parmut[0]
-    sum_parmut_CD[n]  = sum_parmut[1]
-
-with open('OUTPUT/hsmut_sites%s.csv' % suffix, 'w') as f:
-    f.write("HO,CD\n")
-    for n in range(niter):
-        f.write("%d,%d\n" % (sum_hsmut_sites_HO[n],sum_hsmut_sites_CD[n]))
-
-with open('OUTPUT/hsmut_genes%s.csv' % suffix, 'w') as f:
-    f.write("HO,CD\n")
-    for n in range(niter):
-        f.write("%d,%d\n" % (sum_hsmut_genes_HO[n],sum_hsmut_genes_CD[n]))
-
-with open('OUTPUT/parmut%s.csv' % suffix, 'w') as f:
-    f.write("HO,CD\n")
-    for n in range(niter):
-        f.write("%d,%d\n" % (sum_parmut_HO[n],sum_parmut_CD[n]))
+                multihits = [h for h in hits if h > 1]
+                n_multihits = len(multihits)
+    
+                if n_multihits > 0:
+                    sum_hsmut_sites[i] += n_multihits
+                    sum_hsmut_genes[i] += 1
+                    # Check for parallel mutations
+                    for mh in multihits:
+                        # 20 amino acids to choose from (there are 21 total)
+                        xx = np.random.randint(20, size=mh) 
+                        aa, nmut = np.unique(xx, return_counts=True)
+                        parallel = [nm for nm in nmut if nm > 1]
+                        n_parallel = len(parallel)
+                        if n_parallel > 0:
+                            sum_parmut[i] += n_parallel
+                
+        sum_hsmut_sites_HO[n] = sum_hsmut_sites[0]
+        sum_hsmut_sites_CD[n] = sum_hsmut_sites[1]
+        sum_hsmut_genes_HO[n] = sum_hsmut_genes[0]
+        sum_hsmut_genes_CD[n] = sum_hsmut_genes[1]
+        sum_parmut_HO[n]  = sum_parmut[0]
+        sum_parmut_CD[n]  = sum_parmut[1]
+        
+    outfile = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                           'hsmut_sites%s_%d.csv' % (suffix, pct*100))
+    with open(outfile, 'w') as f:
+        f.write("HO,CD\n")
+        for n in range(niter):
+            f.write("%d,%d\n" % (sum_hsmut_sites_HO[n],sum_hsmut_sites_CD[n]))
+    
+    outfile = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                           'hsmut_genes%s_%d.csv' % (suffix, pct*100))
+    with open(outfile, 'w') as f:
+        f.write("HO,CD\n")
+        for n in range(niter):
+            f.write("%d,%d\n" % (sum_hsmut_genes_HO[n],sum_hsmut_genes_CD[n]))
+    
+    outfile = os.path.join(os.path.curdir, 'SIM_OUTPUT', 
+                           'parmut%s_%d.csv' % (suffix, pct*100))
+    with open(outfile, 'w') as f:
+        f.write("HO,CD\n")
+        for n in range(niter):
+            f.write("%d,%d\n" % (sum_parmut_HO[n],sum_parmut_CD[n]))
